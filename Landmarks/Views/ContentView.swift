78d9ace11289c3d8c3d0bcf9d5c60c291c32d04b
Ihsan TOPALOGLU <itopaloglu83@gmail.com>
2021-07-24 20:49:32 -0400

    Initial Commit

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by İhsan TOPALOĞLU on 7/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+
+    var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tag(Tab.featured)
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+
+            LandmarkList()
+                .tag(Tab.list)
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
